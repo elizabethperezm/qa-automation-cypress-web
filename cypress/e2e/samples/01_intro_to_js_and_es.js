391ee6fe09b32ac8b64df3569e57fb98ad0398a1
Elizabeth <elizabeth.perez@wizeline.com>
2025-01-15 09:19:24 -0600

    error on Prettier

--- cypress/e2e/samples/01_intro_to_js_and_es.js
+++ cypress/e2e/samples/01_intro_to_js_and_es.js
@@ -3,5 +3,5 @@ var message = "Hello from ES5!";
 console.log(message);
 
 // ES6 Code
-let newMessage = "Hello from ES6!";
-console.log(newMessage);
+let newMessage="Hello from ES6!"
+console.log(newMessage)
